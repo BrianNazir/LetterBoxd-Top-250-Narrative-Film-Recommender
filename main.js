725ff062360350a6426a5f60450ea43298ee0d3e
BrianNazir <142473169+BrianNazir@users.noreply.github.com>
2025-07-23 20:43:48 -0400

    Changed to a movie suggestion machine. Addded some movies, directors, and genres from LetterBoxd's "Top 250 Narrative Films List"

--- main.js
+++ main.js
@@ -1,1 +1,135 @@
-//Basic Message Generator
+//RNG
+const genRandNum = num => {
+    return Math.floor(Math.random() * num);
+}
+
+//List of 250 movies
+const movies = [
+    'Harakiri',
+    '12 Angry Men',
+    'Come and See',
+    'The Human Condition III: A Soldier\'s Prayer',
+    'Seven Samurai',
+    'High and Low',
+    'The Godfather Part II',
+    'The Shawshank Redemption',
+    'City of God',
+    'Yi Yi',
+    'Parasite',
+    'The Human Condition I: No Greater Love',
+    'Schindler\'s List',
+    'The Godfather',
+    'The Lord of the Rings: The Return of the King',
+    'Ikiru',
+    'Ran',
+    'La Haine',
+    'The Good, the Bad, and the Ugly',
+    'A Brighter Summer Day',
+    'Le Trou',
+    'Cinema Paradiso',
+    'The Dark Knight',
+    'Autumn Sonata',
+    'Grave of the Fireflies',
+    'Neon Genesis Evangelion: The End of Evangelion',
+    'Goodfellas',
+    'Woman in the Dunes',
+    'There Will Be Blood',
+    'The Human Condition: Road to Eternity',
+    'Paths to Glory',
+    'Spirited Away',
+    'Andrei Rublev',
+    'The Apartment',
+    'Apocalypse Now',
+    'The Cranes Are Flying',
+    'Tokyo Story',
+    'Sunset Boulevard',
+    'Incendies',
+    'The Passion of Joan of Arc',
+    'Portrait of a Lady on Fire',
+    'Whiplash',
+    'Fanny & Alexander',
+    'Mishima: A Life in Four Chapters',
+    'Interstellar',
+    'The Battle of Algiers',
+    'Close-Up',
+    'I Am Cuba',
+    'The Ascent',
+    'Spider-Man: Across the Spider-Verse'
+]
+
+
+// List of all the directors of the 250 movies
+const directors = [
+    'Masaki Kobayashi',
+    'Sidney Lumet',
+    'Elem Klimov',
+    'Akira Kurosawa',
+    'Francis Ford Coppola',
+    'Frank Darabont',
+    'Fernando Meirelles',
+    'Edward Yang',
+    'Bong Joon Ho',
+    'Steven Spielberg',
+    'Peter Jackson',
+    'Mathieu Kassovitz',
+    'Sergio Leone',
+    'Jacques Becker',
+    'Giussupe Tornatore',
+    'Christopher Nolan',
+    'Ingmar Bergman',
+    'Isao Takahata',
+    'Hideaki Anno',
+    'Martin Scorsese',
+    'Hiroshi Teshigahara',
+    'Paul Thomas Anderson',
+    'Stanley Kubrick',
+    'Hayao Miyazaki',
+    'Andrei Tarkovsky',
+    'Billy Wilder',
+    'Mikhail Kalatozov',
+    'Yasujiro Ozu',
+    'Denis Villeneuve',
+    'Carl Theodore Dreyer',
+    'Celine Sciamma',
+    'Damien Chazelle',
+    'Paul Schrader',
+    'Gillo Pontecorvo',
+    'Abbas Kiarostami',
+    'Larisa Shepitko',
+    'Kemp Powers',
+    'Justin K. Thompson',
+    'Joaquim Dos Santos'
+]
+
+// List of all the genres among the 250 movies
+const genre = [
+    'action',
+    'history',
+    'drama',
+    'war',
+    'mystery',
+    'thriller',
+    'crime',
+    'comedy',
+    'fantasy',
+    'adventure',
+    'western',
+    'romance',
+    'animation',
+    'sci-fi',
+    'family'
+]
+
+//Letterboxd Top 250 Narrative Films list random options
+let options = {
+    movies: movies,
+    directors: directors,
+    genre: genre
+}
+
+//Array for final output
+let recommendations = [];
+
+const displayMessage = arr => {
+
+}
